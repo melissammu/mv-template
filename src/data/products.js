ce8f8519ed916dd243fa3c4d8abbe0fc6c59649d
Melissa Mejías <melissamejias254@gmail.com>
2026-02-27 00:47:47 -0300

    Fix image path

--- src/data/products.js
+++ src/data/products.js
@@ -2,7 +2,7 @@ export const TIKTOK_VITRINE_URL = "hhttps://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/";
 export const PRODUCTS = [
   {
     name: "Vitrine TikTokShop",
-    image: "/produtos/vitrine.jpeg",
+    image: "/produtos/logo192.jpg",
     url: "https://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/",
   },
 ];
