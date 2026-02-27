ffcf7fbc2377e91a53516746c74c4937ef5e5c98
Melissa Mejías <melissamejias254@gmail.com>
2026-02-27 02:30:57 -0300

    Clean project - only vitrine button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import SitePage from "./pages/sitePages";
-import ProductCatalogPage from "./pages/ProductCatalogPage";
-
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SitePage />} />
-      <Route path="/productos" element={<ProductCatalogPage />} />
     </Routes>
   );
 }
--- src/pages/sitePages.js
+++ src/pages/sitePages.js
@@ -1,68 +1,26 @@
-import { ENVY_CONFIG } from "../data/config";
-import { Link } from "react-router-dom";
-import "./sitePages.css";
-
-import {
-  FaYoutube,
-  FaPinterest,
-  FaEnvelope,
-  FaInstagram,
-  FaWhatsapp,
-  FaTiktok,
-} from "react-icons/fa6";
-
-import { sites } from "../data/sites";
-
 export default function SitePage() {
-  const { profile, buttons, social } = ENVY_CONFIG; // si no usas profile/buttons por ahora, está bien
-
   return (
- <div className="site-container">
-  <div className="site-card">
-    <img src={profile.avatar} alt="avatar" className="site-avatar" />
-
-   <div className="social-icons">
-  {social?.map((item, index) => {
-    let Icon = FaEnvelope;
-
-    if (item.type === "instagram") Icon = FaInstagram;
-    if (item.type === "tiktok") Icon = FaTiktok;
-    if (item.type === "youtube") Icon = FaYoutube;
-    if (item.type === "pinterest") Icon = FaPinterest;
-    if (item.type === "whatsapp") Icon = FaWhatsapp;
-
-    return (
-     <a key={index} href={item.url} target="_blank" rel="noreferrer">
-        <Icon />
+    <div style={{ 
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    }}>
+      <a
+        href="https://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          backgroundColor: "#e6bcbb",
+          color: "#fff",
+          padding: "15px 25px",
+          borderRadius: "30px",
+          textDecoration: "none",
+          fontWeight: "bold"
+        }}
+      >
+        👉 Clique agora - Vitrine TikTokShop
       </a>
-    );
-  })}
-</div>
-   {/* Nombre + Bio */}
-      <h1 className="site-name">{profile.name}</h1>
-      <p className="site-bio">{profile.tagline}</p>
-
-      {/* BOTONES */}
-      <div className="buttons">
-        {buttons?.map((btn, index) =>
-          btn.type === "internal" ? (
-            <Link key={index} to={btn.route} className="site-button">
-              {btn.label}
-            </Link>
-          ) : (
-            <a
-              key={index}
-              href={btn.url}
-              target="_blank"
-              rel="noreferrer"
-              className="site-button"
-            >
-              {btn.label}
-            </a>
-          )
-        )}
-      </div>
     </div>
-  </div>
-);
- }
+  );
+}
