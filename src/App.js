c8829ecd6f24272f3e56bc9332b1124f4e5b02e8
Melissa Mejías <melissamejias254@gmail.com>
2026-02-27 02:20:58 -0300

    fix build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import SitePage from "./pages/sitePages";
 import ProductCatalogPage from "./pages/ProductCatalogPage";
-import AdminProductsPage from "./pages/AdminProductsPage";
-import AdminLoginPage from "./pages/AdminLoginPage";
-import AdminGuard from "./components/AdminGuard";
+
 export default function App() {
   return (
-      <Routes>
-  <Route path="/" element={<SitePage />} />
-  <Route path="/productos" element={<ProductCatalogPage />} />
-
-  <Route path="/Admin/login" element={<AdminLoginPage />} />
-  <Route
-    path="/Admin/products"
-    element={
-      <AdminGuard>
-        <AdminProductsPage />
-      </AdminGuard>
-    }
-  />
-</Routes>
-  )
- }
+    <Routes>
+      <Route path="/" element={<SitePage />} />
+      <Route path="/productos" element={<ProductCatalogPage />} />
+    </Routes>
+  );
+}
--- src/data/products.js
+++ src/data/products.js
@@ -1,8 +1,1 @@
 export const TIKTOK_VITRINE_URL = "https://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/";
-export const PRODUCTS = [
-  {
-    name: "Vitrine TikTokShop",
-    image: "produtos/prod1.jpg",
-    url: "https://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/",
-  },
-];
