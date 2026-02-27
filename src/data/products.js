0f62bf45f4d9b4586b7ddfb048562bdff1079114
Melissa Mejías <melissamejias254@gmail.com>
2026-02-27 00:40:00 -0300

    add products file

--- src/data/products.js
+++ src/data/products.js
@@ -2,7 +2,7 @@ export const TIKTOK_VITRINE_URL = "hhttps://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/";
 export const PRODUCTS = [
   {
     name: "Vitrine TikTokShop",
-    image: "/produtos/vitrine.jpg",
+    image: "/produtos/vitrine.jpeg",
     url: "https://vt.tiktok.com/ZS9egfJnuCwW7-4veHy/",
   },
 ];
